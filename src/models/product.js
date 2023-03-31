cdb11ef8c14545d2026d63da84b639bc684adf87
MMMAAARRRTTT <mctulio.2@gmail.com>
2023-03-31 01:41:48 -0700

    fix: fixed product.js

--- src/models/product.js
+++ src/models/product.js
@@ -1,47 +1,27 @@
-"use strict";
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Products", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      prod_type: {
-        type: Sequelize.ENUM,
-        values: ["cosmetic", "beauty", "cleaning"],
-        defaultValue: "cosmetic",
-      },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      price: {
-        type: Sequelize.FLOAT,
-        defaultValue: 0,
-        allowNull: false,
-      },
-      instock: {
-        type: Sequelize.INTEGER,
-        defaultValue: 0,
-        allowNull: false,
-      },
-      image: {
-        type: Sequelize.STRING,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Products");
-  },
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Product.init({
+    prod_type: DataTypes.STRING,
+    name: DataTypes.STRING,
+    price: DataTypes.FLOAT,
+    instock: DataTypes.INTEGER,
+    image: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Product',
+  });
+  return Product;
 };
