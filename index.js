5ede0404218661d4fd19c99014f5b41a92cbfab6
MMMAAARRRTTT <mctulio.2@gmail.com>
2023-03-31 01:35:31 -0700

    fix: fixed index to wqork with sequelize

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-const express = require("express");
-const morgan = require("morgan");
-const db = require("./src/models");
-import apiRouter from "./src/routes";
 
-const app = express();
-const port = 3000;
+const express = require('express')
+const morgan = require('morgan')
+const db = require('./src/models')
+import apiRouter from './src/routes';
 
-db.sequelize
-  .sync()
-  .then(() => {
+const app = express()
+const port = 3001
+
+db.sequelize.sync()
+    .then(() => {
     console.log("Synced db.");
-  })
-  .catch((err) => {
+    })
+    .catch((err) => {
     console.log("Failed to sync db: " + err.message);
-  });
+    });
 
-app.use(morgan("dev"));
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(morgan('dev'))
+app.use(express.json());
+app.use(apiRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+    console.log(`Example app listening on port ${port}`)
+})
+
+//db.sequelize.sync({ force: true }).then(() => {
+//    console.log("Drop and re-sync db.");
+//    });
